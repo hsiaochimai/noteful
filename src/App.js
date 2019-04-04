737d7d7161f4c2fadd545a2d2d87ca5b4e2766ed
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-04-04 16:11:51 +0300

    WIP; folder route

--- src/App.js
+++ src/App.js
@@ -15,10 +15,16 @@ class App extends Component {
 
   render() {
     console.log('App props:', this.props)
+    //shallow copy
+    const storeCopy = { ...this.props.STORE }
+    const pageProps = {
+      data: storeCopy,
+    }
+    console.log('Page props:', pageProps)
     return (
       <div className="App">
         <Header />
-        <MainPage data={this.props.STORE} />
+        <MainPage {...pageProps} />
 
       </div>
     );
