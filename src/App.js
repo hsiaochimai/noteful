eeba75c93623251eec6720f759b66f83d97a8d1a
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-04-09 14:32:38 +0300

    parsing location manually

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import MainPage from "./MainPage";
 import Header from "./Header";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch, matchPath } from "react-router-dom";
 import "./App.css";
 import FolderEdit from "./FolderEdit";
 import NotePage from './NotePage'
 import NotefulContext from './NotefulContext'
 import { withRouter } from "react-router-dom"
+import rp from 'route-parser'
 
 class App extends Component {
   constructor(props) {
@@ -17,15 +18,19 @@ class App extends Component {
       ...this.props.STORE
     };
   }
- 
-  componentDidUpdate(){
-    const {match:params} = this.props
+
+  componentDidUpdate() {
+    const { match: params } = this.props
     console.log('SMTHNG CHANGED', this.props)
+    const r = new rp("/folder/:folderID")
+    const routeParams = r.match(this.props.location.pathname)
+    //routeParams now holds folderID so you can setState
+    debugger
   }
 
   componentDidMount() {
     console.log("App mounted");
-    
+
   }
 
   getFolderID(routerProps) {
@@ -77,42 +82,45 @@ class App extends Component {
       <div className="App">
         <Header />
         <NotefulContext.Provider value={contextValue}>
-          <Route
-            exact
-            path="/"
-            render={routerProps =>
-              <MainPage key="FolderList" {...routerProps} />
-            }
-          />
-          <Route
-            path="/folder/:folderID"
-            render={routerProps => {
-              // debugger
-              
-              const folderID = routerProps.match.params.folderID
-              notes = notes.filter(note => note.folderId === folderID);
-              return <MainPage key="FolderList" {...routerProps} />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={routerProps =>
+                <MainPage key="FolderList" {...routerProps} />
+              }
+            />
+            <Route
+              exact
+              path="/folder/:folderID"
+              render={routerProps => {
+                // debugger
 
-            }}
-        />
+                const folderID = routerProps.match.params.folderID
+                notes = notes.filter(note => note.folderId === folderID);
+                return <MainPage key="FolderList" {...routerProps} />
 
-          <Route
-            path="/addFolder"
-            render={routerProps => (
-              <FolderEdit
-                {...{ ...routerProps, onFolderAdd: this.onFolderAdd }}
-              />
-            )}
-          />
-          <Route
-            path="/note/:noteID"
-            render={routerProps => {
-              const noteID = routerProps.match.params.noteID
-              const notes = notes.filter(note => note.id === noteID)
+              }}
+            />
 
-              console.log(`folder id is`, folders)
-              return <NotePage {...routerProps} data={{ folders, notes }} />
-            }} />
+            <Route
+              path="/addFolder"
+              render={routerProps => (
+                <FolderEdit
+                  {...{ ...routerProps, onFolderAdd: this.onFolderAdd }}
+                />
+              )}
+            />
+            <Route
+              path="/note/:noteID"
+              render={routerProps => {
+                const noteID = routerProps.match.params.noteID
+                const notes = notes.filter(note => note.id === noteID)
+
+                console.log(`folder id is`, folders)
+                return <NotePage {...routerProps} data={{ folders, notes }} />
+              }} />
+          </Switch>
 
         </NotefulContext.Provider>
       </div>
