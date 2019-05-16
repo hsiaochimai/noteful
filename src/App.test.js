96ca743523a9504bdcd5f3ac5401c3f355a57f4e
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-05-16 17:59:34 +0300

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from 'history';
+const history = createBrowserHistory();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Router history={history}>
+      <App key='App' />
+    </Router>,
+    div);
   ReactDOM.unmountComponentAtNode(div);
 });
